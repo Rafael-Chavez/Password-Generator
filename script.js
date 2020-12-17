e7890822e79fcd9b919726c1cfd948e1122afff9
Rafael Chavez <chavezrafaelbw@gmail.com>
2020-12-17 08:35:05 -0500

    added variables to the homework for the case characters

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var generateBtn = document.querySelector("#generate");
 
 
 
-  // var upperCase = ["ABCDEFGHJILKLMNOPQRSTUVWXYZ"];
+  // var upperCase = ["A,B,C,D,E,F,G,H,J,I,L,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"];
   // var numeric = [];
   // var specialCharacters = [!@#$%^&*()]
 // Write password to the #password input
@@ -13,9 +13,17 @@ function writePassword() {
 
   passwordText.value = password;
 
+  function generatePassword(){
+    var upperCase = confirm("include upper case letters?");
+    var lowerCase = confirm("include lower case letters?");
+    var numberCase = confirm("include numbers?");
+    var symbolCase = confirm("include symbols?");
+    
   
+
+  }
 }
 
-}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
