2851573f5eb83aa37afdb9081c2e6c2d86c50798
Rafael Chavez <chavezrafaelbw@gmail.com>
2020-12-17 08:54:14 -0500

    fixed the variables so they're independent instead of one single string value.

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var passwordText = document.querySelector("#password");
 
   var allChar = {
-   upperCaseValue = ["A,B,C,D,E,F,G,H,J,I,L,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"],
-   lowerCaseValue = ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"],
-   numeric = [0,1,2,3,4,5,6,7,8,9],
-   specialCharacters = ["!,@,#,$,%,^,&,*"],
+   upperCaseValue : ["A","B","C","D","E","F","G","H","J","I","L","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
+   lowerCaseValue : ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
+   numeric : [0,1,2,3,4,5,6,7,8,9],
+   specialCharacters : ["!","@","#","$","%","^","&","*"],
   }
 // Write password to the #password input
 function writePassword() {
@@ -42,9 +42,9 @@ function writePassword() {
       alert("You need one of these choices.");
       return;
     }
-
   }
-}
+  
+
 
 
 // Add event listener to generate button
