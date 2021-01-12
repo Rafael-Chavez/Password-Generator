adc3bd73db7ea0cd46150c6330398696ae96890e
Rafael Chavez <chavezrafaelbw@gmail.com>
2021-01-12 03:49:21 -0500

    resetting the code from mostly scratch

--- script.js
+++ script.js
@@ -1,51 +1,35 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var passwordText = document.querySelector("#password");
-
-  var allChar = {
-   upperCaseValue : ["A","B","C","D","E","F","G","H","J","I","L","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
-   lowerCaseValue : ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
-   numeric : [0,1,2,3,4,5,6,7,8,9],
-   specialCharacters : ["!","@","#","$","%","^","&","*"],
-  }
+var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+var specialChar = ['#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','['];
+var num = [0,1,2,3,4,5,6,7,8,9];
+var passwordLength = "";
+  
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-}
-  // passwordText.value = password;
-
-  function generatePassword(){
-    var upperCase = confirm("include upper case letters?");
-    var lowerCase = confirm("include lower case letters?");
-    var numberCase = confirm("include numbers?");
-    var symbolCase = confirm("include symbols?");
+  
+   passwordText.value = password;
+}  
+  prompt("choose length of password 8-128");{
+    if(pwdLength < 8 || pwdLength > 128){
+    alert("please choose a number between 8-128");}
+    return;
+  }
+  if(uppercase{
+    allChar.concat(upperCase);
+  })
+      console.log(pwdLength);
+    }
     
-    var pwdLength = prompt("How many characters would you like to have between 8-128?");
-    if (pwdLength < 8 || pwdLength > 128){
-      alert("pick a length for your password between 8-128.");
-      return;
-    }
-    else if (upperCase){
-      allChar.concat(upperCaseValue);
-    }
-    else if (lowerCase){
-      allChar.concat(lowerCaseValue);
-    }
-    else if (numberCase){
-      all.char.concat(numeric);
-    }
-    else if (symbolCase){
-      all.char.concat(specialCharacters);
-    }
-    else (!upperC && !lowerC && !number && !symbol){
-      alert("You need one of these choices.");
-      return;
-    }
   }
   
 
 
-
+function generatePassword(){
+  return "temporary password"
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
